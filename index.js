9c29cb2744f640b0ca527f89b9672b2f0d79f116
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-09 20:39:02 +1000

    Move h1-h6 tags from shazam across

--- index.js
+++ index.js
@@ -10,6 +10,17 @@
 
 **/
 
+var tag = exports.tag = require('./tag');
+var SUPPORTED_TAGS = [
+  // headings
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+];
+
 exports.slide = require('./slide');
 exports.html = require('./html');
 exports.img = require('./img');
+
+// export the tag creators
+SUPPORTED_TAGS.forEach(function(tagName) {
+  shazam[tagName] = tag(tagName);
+});
