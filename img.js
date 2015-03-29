637f7f75df2b7e56eaf2e90e24b3d88212c9471c
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-29 14:36:43 +1100

    Added some reference docs for the img creator

--- img.js
+++ img.js
@@ -1,9 +1,17 @@
 var crel = require('crel');
 var Slide = require('./slide');
 
+/**
+  ### `img(url, opts?) => Slide`
+
+  Create a slide that contains a single image which is displayed using
+  a `backround-image` CSS style.
+
+**/
 module.exports = function(url, opts) {
   var slide = new Slide();
 
+  console.log('creating an img with url: ', url);
   // create an image to trigger loading
   var img = crel('img', {
     src: url
